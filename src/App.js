72272a7a461d1813cfaf8633149b33457fa7f187
SangJun Park <amang322@gmail.com>
2021-07-28 15:15:50 +0900

    Execise immer api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useMemo, createContext } from "react";
+import produce from "immer";
 import Wrapper from "./Wrapper";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
@@ -36,29 +37,40 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        //...state,
-        //inputs: initialState.inputs,
-        //users: [...state.users, action.user],
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // {
+    //...state,
+    //inputs: initialState.inputs,
+    //users: [...state.users, action.user],
+    //users: state.users.concat(action.user),
+    // };
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id
-            ? {
-                ...user,
-                active: !user.active,
-              }
-            : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id
+    //       ? {
+    //           ...user,
+    //           active: !user.active,
+    //         }
+    //       : user
+    //   ),
+    // };
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
     default:
       return state;
   }
